622a013fd1cebec44c04f8f005b455381d0b4aff
noornabi07 <noornabiprogram07@gmail.com>
2023-09-08 11:09:34 -0700

    Login page setup and design

--- src/Routers/Routes.jsx
+++ src/Routers/Routes.jsx
@@ -3,6 +3,7 @@ import Main from "../Components/MainLayout/Main";
 import Home from "../Components/Home/Home";
 import Menu from "../Components/Menu/Menu/Menu";
 import Order from "../Components/Order/Order/Order";
+import Login from "../Components/Login/Login";
 
 const router = createBrowserRouter([
     {
@@ -20,6 +21,10 @@ const router = createBrowserRouter([
             {
                 path: 'order/:category',
                 element: <Order></Order>
+            },
+            {
+                path: 'login',
+                element: <Login></Login>
             }
         ]
     }
