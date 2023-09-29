69209d43683ebb3cd74d5cda29642f2864ceaa9e
noornabi07 <noornabiprogram07@gmail.com>
2023-09-29 18:47:04 +0600

    my carts show all data in the table

--- src/Components/Dashboard/MyCart/MyCart.jsx
+++ src/Components/Dashboard/MyCart/MyCart.jsx
@@ -1,14 +1,58 @@
 import React from 'react';
 import { Helmet } from 'react-helmet-async';
+import useCarts from '../../../Hooks/useCarts';
+import { AiFillDelete } from 'react-icons/Ai';
+import { FaTrashAlt } from 'react-icons/fa';
 
 
 const MyCart = () => {
+    const [cart] = useCarts();
+    const total = cart.reduce((sum, item) => item.price + sum, 0)
     return (
         <div>
             <Helmet>
                 <title>Food Boss | My Cart</title>
             </Helmet>
-            <h1>My cart page here</h1>
+            <div className='flex justify-evenly text-lg font-bold text-white'>
+                <h1>Total Items: {cart.length}</h1>
+                <h1>Total Prices: ${total}</h1>
+                <button className="btn btn-warning btn-sm">Pay</button>
+            </div>
+
+            <div className="overflow-x-auto border mt-5">
+                <table className="table">
+                    {/* head */}
+                    <thead className='font-semibold'>
+                        <tr className='text-lg text-green-500'>
+                            <th>#</th>
+                            <th>Item Image</th>
+                            <th>Item Name</th>
+                            <th>Item Price</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody className='text-white'>
+                        {
+                            cart.map((item, index) => <tr key={item._id}>
+                                <th>{index + 1}</th>
+                                <td>
+                                    <div className="avatar">
+                                        <div className="mask mask-squircle w-12 h-12">
+                                            <img src={item.image} alt="Avatar Tailwind CSS Component" />
+                                        </div>
+                                    </div>
+                                </td>
+                                <td>{item.name}</td>
+                                <td>${item.price}</td>
+                                <td><button className='btn btn-sm btn-ghost'><FaTrashAlt className='text-xl'></FaTrashAlt></button></td>
+                            </tr>)
+                        }
+
+                    </tbody>
+                </table>
+            </div>
+
+
         </div>
     );
 };
--- src/Components/MainLayout/Dashboard.jsx
+++ src/Components/MainLayout/Dashboard.jsx
@@ -10,7 +10,7 @@ const Dashboard = () => {
         <div>
             <div className="drawer lg:drawer-open">
                 <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
-                <div className="drawer-content flex flex-col items-center justify-center">
+                <div className="drawer-content bg-slate-500 flex flex-col items-center justify-center">
                     {/* Page content here */}
                     <Outlet></Outlet>
                     <label htmlFor="my-drawer-2" className="btn btn-primary drawer-button lg:hidden">Open drawer</label>
@@ -18,17 +18,17 @@ const Dashboard = () => {
                 </div>
                 <div className="drawer-side">
                     <label htmlFor="my-drawer-2" className="drawer-overlay"></label>
-                    <ul className="menu p-4 w-80 min-h-full bg-base-200 text-base-content">
+                    <ul className="menu p-4 w-80 min-h-full bg-[#D1A054] text-white">
                         {/* Sidebar content here */}
-                        <li><Link to="/"><FaHome></FaHome>User Home</Link></li>
-                        <li><Link><FaCalendarAlt></FaCalendarAlt>Reservation</Link></li>
-                        <li><Link><FaWallet></FaWallet> Payment History</Link></li>
+                        <li><Link to="/dashboard/userhome"><FaHome></FaHome>User Home</Link></li>
+                        <li><Link to="/dashboard/reservation"><FaCalendarAlt></FaCalendarAlt>Reservation</Link></li>
+                        <li><Link to="/dashboard/history"><FaWallet></FaWallet> Payment History</Link></li>
                         <li><Link to="/dashboard/mycart"><FaShoppingCart></FaShoppingCart> My Cart</Link></li>
                         <div className="divider"></div>
                         <li><Link to="/"><FaHome></FaHome>HOME</Link></li>
                         <li><Link to="/menu"><BiMenu></BiMenu>MENU</Link></li>
-                        <li><Link><AiFillShopping></AiFillShopping>SHOP</Link></li>
-                        <li><Link><BiSolidContact></BiSolidContact>CONTACT</Link></li>
+                        <li><Link to="/dashboard/shop"><AiFillShopping></AiFillShopping>SHOP</Link></li>
+                        <li><Link to="/dashboard/contact"><BiSolidContact></BiSolidContact>CONTACT</Link></li>
                         
                     </ul>
 
